737f016a08456da6367c164f02e96ab1e545ad35
Achrafcodes <esssoussygamer@gmail.com>
2023-04-03 03:01:04 +0000

    d

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,25 +1,46 @@
-import { useState } from "react";
-import Data from "../MemesData";
-let Main = () => {
-  let [memeImg, setmemeImg] = useState("");
-  let Handleclick = function () {
-    let memesArray = Data.data.memes;
-    const Randommeme = Math.floor(Math.random() * memesArray.length);
-    let { url } = memesArray[Randommeme];
-    return setmemeImg(url);
-  };
+import React from "react";
+import memesData from "../MemesData";
+
+export default function Meme() {
+  /**
+   * Challenge:
+   * 1. Set up the text inputs to save to
+   *    the `topText` and `bottomText` state variables.
+   * 2. Replace the hard-coded text on the image with
+   *    the text being saved to state.
+   */
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
   return (
     <main>
       <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button onClick={Handleclick} className="form--button">
+        <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
-      </div>{" "}
-      <img className="meme" src={memeImg} alt="" />
+      </div>
+      <div className="meme">
+        <img alt="" src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">One does not simply</h2>
+        <h2 className="meme--text bottom">Walk into Mordor</h2>
+      </div>
     </main>
   );
-};
-// let's start the section
-export default Main;
+}
